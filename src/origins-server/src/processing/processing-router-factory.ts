7f8f7a2c7b88045e19039771271050ae9a85d95e
Shaun <smphayward@gmail.com>
2022-10-25 23:37:04 -0400

    #50 updated processing request response to be GeneralResponse format.

--- src/origins-server/src/processing/processing-router-factory.ts
+++ src/origins-server/src/processing/processing-router-factory.ts
@@ -1,9 +1,8 @@
 import express, { Router } from "express";
-import * as path from 'path';
+import * as path from "path";
 import { CollectionProvider } from "origins-common/collections";
 import { ProcessingProvider } from "./ProcessingProvider";
-
-
+import { GeneralResponse } from "origins-common";
 
 export const createProcessingRouter = (
   collectionProvider: CollectionProvider,
@@ -11,38 +10,48 @@ export const createProcessingRouter = (
 ): Router => {
   const router = express.Router();
 
-  router
-    .post(['/:collectionId/*', '/:collectionId'], async (req, res) => {
+  router.post(["/:collectionId/*", "/:collectionId"], async (req, res) => {
+    const collectionId = req.params.collectionId;
 
-      const collectionId = req.params.collectionId;
+    console.log(
+      `Received processing request for collection '${collectionId}'.`
+    );
 
-      console.log(`Received processing request for collection '${collectionId}'.`);
+    // First, does the collection exist?
+    const result = await collectionProvider.get(collectionId);
+    if (!result.success || !result.document) {
+      return res.status(404).send({
+        success: true,
+        statusCode: 200,
+        message: `Collection '${collectionId}' not found.`,
+      });
+    }
 
-      // First, does the collection exist?
-      const result = await collectionProvider.get(collectionId);
-      if (!result.success || !result.document) {
-        return res.status(404).send(`Collection '${collectionId}' not found.`);
-      }
+    // Second, strip the url
+    const urlPortionToStrip = "/" + collectionId;
+    const relativePath = req.url.substring(urlPortionToStrip.length);
 
-      // Second, strip the url
-      const urlPortionToStrip = "/" + collectionId;
-      const relativePath = req.url.substring(urlPortionToStrip.length);
+    // Third, make the processing request
+    console.log(
+      `Processing '${relativePath}' in collection '${collectionId}'.`
+    );
+    const decodedRelativePath = decodeURI(relativePath);
+    await processingProvider.processPath(
+      result.document,
+      decodedRelativePath,
+      -1
+    ); // TODO: get depth from header
+    console.log("Processing completed.");
 
-      // Third, make the processing request
-      console.log(`Processing '${relativePath}' in collection '${collectionId}'.`);
-      const decodedRelativePath = decodeURI(relativePath);
-      await processingProvider.processPath(result.document, decodedRelativePath, -1); // TODO: get depth from header
-      console.log('Processing completed.');
+    return res.status(200).send({
+      success: true,
+      statusCode: 200,
+      message: "Processing completed.",
+    });
 
-      return res.status(200).send("Processing completed.");
-
-      // TODO: Put this in a queue and offload it to a worker process?
-      // For now, we'll just do it in process with blocking
-
-
-    })
-
+    // TODO: Put this in a queue and offload it to a worker process?
+    // For now, we'll just do it in process with blocking
+  });
 
   return router;
-
-}
+};
